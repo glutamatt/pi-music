8cffee32526074e6c6778a60795799ab3fd5544d
Glutamatt <glutamatt@live.fr>
2013-12-18 15:01:25 +0100

    Infinite Scroll refacto and all albums route

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,19 @@ pimusicApp.filter('groupRow', function() {
   };
 });
 
+/* INFINITE SCROLL HANDLER */
+pimusicApp.factory( 'infiniteScrollHandler',  [ '$filter', function( $filter ){
+  return function( $scope, entities, sliceSize, methodeName ) {
+    var offset = 0 , slice = sliceSize ;
+    $scope.tilesGroups2 = [] ;
+    $scope[methodeName] = function(){ 
+      angular.forEach( $filter('groupRow')(entities.slice(offset, offset+slice ), 2),function(g) {
+        $scope.tilesGroups2.push(g) ;
+      }) ; offset=offset+slice ; } ;
+    $scope[methodeName]() ;
+  } ;
+}] ) ;
+
 /* L O A D E R */
 pimusicApp.factory('mEntitiesLoader', ['$http' , function ($http) {
   var loadcb = { progress: [] ,finish: [] } ;
@@ -77,9 +90,10 @@ pimusicApp.factory('mEntities', [ 'mEntitiesLoader' , function(mEntitiesLoader){
         musicIndex.artists[artistId] = eArtist ;
 
       });},
-		getArtists: function() { return iToa(musicIndex.artists) ;  } ,
-		getAlbums: function(artistId) { return iToa(musicIndex.artists[artistId].albums) ; },
-		getSongs: function(albumId){ return iToa(musicIndex.albums[albumId].songs) ; }
+		getAllArtists: function() { return iToa(musicIndex.artists) ;  } ,
+		getAlbumsByArtistId: function(artistId) { return iToa(musicIndex.artists[artistId].albums) ; },
+    getAllAlbums: function() { return iToa(musicIndex.albums) ; },
+		getSongsByAlbumId: function(albumId){ return iToa(musicIndex.albums[albumId].songs) ; }
 	};
 } ] ) ;
 
@@ -90,9 +104,11 @@ pimusicApp.config(['$routeProvider',function($routeProvider) {
       when('/artists', {templateUrl: prefix_partial + 'tileset.html', controller: 'ArtistsCtrl', resolve:resolve}).
       when('/artists/:artistId',  {templateUrl: prefix_partial + 'tileset.html', controller: 'AlbumsCtrl', resolve:resolve}).
       when('/albums/:albumId',  {templateUrl: prefix_partial +  'songs.html',   controller: 'SongsCtrl', resolve:resolve}).
+      when('/albums',           {templateUrl: prefix_partial +  'tileset.html',   controller: 'AlbumsCtrl', resolve:resolve}).
       when('/play/songs/:songId',  {templateUrl: prefix_partial +  'songs.html',   controller: 'SongsCtrl', resolve:resolve}).
       otherwise({redirectTo: '/artists'});
   }]);
+
 
 /* C O N T R O L S */
 var pimusicControllers = angular.module('pimusicControllers', [ 'infinite-scroll' ] );
@@ -112,24 +128,21 @@ pimusicControllers.controller('LoadCtrl', // preLoad
     mEntitiesLoader.onLoadFinish(function( ) { $scope.finish = true ;}) ;
 }]);
 pimusicControllers.controller('ArtistsCtrl', // Artists
-  ['$scope', '$filter' , 'mEntities', function ($scope, $filter, mEntities ) {
-    var artists = mEntities.getArtists() , offset = 0 , slice = 20 ;
-    $scope.tilesGroups2 = [] ;
-    $scope.addSlice = function(){ 
-      angular.forEach( $filter('groupRow')(artists.slice(offset, offset+slice ), 2),function(g) {
-        $scope.tilesGroups2.push(g) ;
-      }) ; offset=offset+slice ; } ;
-    $scope.addSlice() ;
+  ['$scope', 'mEntities', 'infiniteScrollHandler' , function ($scope, mEntities, infiniteScrollHandler ) {
+    infiniteScrollHandler( $scope,  mEntities.getAllArtists(), 20 , 'addSlice' ) ;
     $scope.tileLinkUrl = function( tile ) { return '#/artists/' + tile.id ; } ;
 }]);
 pimusicControllers.controller('AlbumsCtrl', // Albums
-  ['$scope', '$filter' , 'mEntities', '$routeParams' , function ($scope, $filter, mEntities, $routeParams ) {
-    $scope.tilesGroups2 = $filter('groupRow')( mEntities.getAlbums($routeParams.artistId) , 2 );
+  ['$scope' , 'mEntities', '$routeParams' , 'infiniteScrollHandler', function ($scope, mEntities, $routeParams , infiniteScrollHandler ) {
+    var albums = ($routeParams.artistId) ? 
+      mEntities.getAlbumsByArtistId($routeParams.artistId) : 
+      mEntities.getAllAlbums() ;
+    infiniteScrollHandler( $scope,  albums , 20 , 'addSlice' ) ;
     $scope.tileLinkUrl = function( tile ) { return '#/albums/' + tile.id ; } ;
 }]);
 pimusicControllers.controller('SongsCtrl', // Songs
   ['$scope', 'mEntities', '$routeParams', 'mPlayer' , function ($scope, mEntities, $routeParams, mPlayer  ) {
-  	$scope.songs = mEntities.getSongs($routeParams.albumId) ;
+  	$scope.songs = mEntities.getSongsByAlbumId($routeParams.albumId) ;
     $scope.playSong = function(song) { mPlayer.startPlay( song.id , function() {
       console.log('song started : controller callback() !') ;
     }) ; } ;
